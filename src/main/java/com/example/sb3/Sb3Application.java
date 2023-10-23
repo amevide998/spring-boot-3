08becb19eded3e452662971b2ff6b56b6e2361af
amevide998 <hdsvidi@gmail.com>
2023-10-24 00:57:27 +0700

    update

--- src/main/java/com/example/sb3/Sb3Application.java
+++ src/main/java/com/example/sb3/Sb3Application.java
@@ -1,13 +1,65 @@
 package com.example.sb3;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Slf4j
 public class Sb3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sb3Application.class, args);
+
+        var cs = new DefaultCustomerService();
+        var all = cs.all();
+        all.forEach(c-> log.info(c.toString()));
+
     }
 
 }
+
+@Slf4j
+    class DefaultCustomerService {
+
+    private final DataSource dataSource;
+
+    DefaultCustomerService() {
+        var dataSource = new DriverManagerDataSource(
+            "jdbc:postgresql://db.wpjfqirouzfglvfdhfba.supabase.co:5432/postgres",
+            "postgres",
+            "0n9Evswhohpk4pKe"
+        );
+
+        dataSource.setDriverClassName(Driver.class.getName());
+        this.dataSource = dataSource;
+    }
+
+    Collection<Customer> all(){
+        var customers = new ArrayList<Customer>();
+        try {
+            try (var connection = this.dataSource.getConnection()) {
+                try (var stmt = connection.createStatement()) {
+                    try (var rs = stmt.executeQuery("SELECT * FROM customers")) {
+                        while (rs.next()) {
+                            customers.add(new Customer(
+                                    rs.getInt("id"),
+                                    rs.getString("name")
+                            ));
+                        }
+                    }
+                }
+            }
+        }catch (Exception e){
+            log.error("something wrong", e);
+        }
+        return customers;
+    }
+}
+
+record Customer (Integer id, String name) {}
