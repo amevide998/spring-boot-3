5bd505ffab9106dc9f1e88ab4a825619c15f5b06
amevide998 <hdsvidi@gmail.com>
2023-10-24 16:54:45 +0700

    update example 2

--- src/main/java/com/example/sb3/Sb3Application.java
+++ src/main/java/com/example/sb3/Sb3Application.java
@@ -3,21 +3,47 @@ package com.example.sb3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.util.Assert;
 
 import javax.sql.DataSource;
-import java.sql.Driver;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.sql.*;
+import java.util.*;
 
 @Slf4j
 public class Sb3Application {
 
     public static void main(String[] args) {
+        var dataSource = new DriverManagerDataSource(
+                "jdbc:postgresql://db.wpjfqirouzfglvfdhfba.supabase.co:5432/postgres",
+                "postgres",
+                "0n9Evswhohpk4pKe"
+        );
+        dataSource.setDriverClassName(Driver.class.getName());
 
-        var cs = new DefaultCustomerService();
+        var template = new JdbcTemplate(dataSource);
+        template.afterPropertiesSet();
+
+        var ptm = new DataSourceTransactionManager(dataSource);
+
+        var tt = new TransactionTemplate(ptm);
+        tt.afterPropertiesSet();
+
+
+        var cs = new DefaultCustomerService(template, tt);
+
+        var yamato = cs.add("yamato");
         var all = cs.all();
+        Assert.state(all.contains(yamato), () -> "yamato not found");
+
         all.forEach(c-> log.info(c.toString()));
 
     }
@@ -25,41 +51,75 @@ public class Sb3Application {
 }
 
 @Slf4j
-    class DefaultCustomerService {
+class DefaultCustomerService {
+    private final JdbcTemplate template;
+    private final TransactionTemplate tt;
 
-    private final DataSource dataSource;
+    private final RowMapper<Customer> rowMapper = (rs, rowNum)
+            -> new Customer(rs.getInt("id"), rs.getString("name"));
 
-    DefaultCustomerService() {
-        var dataSource = new DriverManagerDataSource(
-            "jdbc:postgresql://db.wpjfqirouzfglvfdhfba.supabase.co:5432/postgres",
-            "postgres",
-            "0n9Evswhohpk4pKe"
+    DefaultCustomerService(JdbcTemplate template, TransactionTemplate tt) {
+        this.template = template;
+        this.tt = tt;
+
+    }
+
+    Customer add(String name){
+
+
+
+        var al = new ArrayList<Map<String, Object>>();
+        var hm = new HashMap<String, Object>() {};
+        hm.put("id", Long.class);
+        al.add(hm);
+        var keyholder = new GeneratedKeyHolder(al);
+        log.info("cek keyholder {}", keyholder.getKeyList());
+        this.template.update(con -> {
+                            PreparedStatement ps = con.prepareStatement("insert into customers (name) values(?)", Statement.RETURN_GENERATED_KEYS);
+                            ps.setString(1, name);
+                            log.info("cek keyholder {}", keyholder.getKeyList());
+                            return ps;
+                },
+                keyholder
         );
+        var generatedId = Objects.requireNonNull(keyholder.getKeys()).get("id");
+        log.info("generated {}", generatedId);
+        Assert.state(generatedId instanceof Number, () -> "generatedId is not a number");
+        Number number = (Number) generatedId;
+        return byId(number.intValue());
+    }
 
-        dataSource.setDriverClassName(Driver.class.getName());
-        this.dataSource = dataSource;
+    Customer byId(Integer id){
+        return this.template.queryForObject("select id, name from customers where id = ?", this.rowMapper, id);
     }
 
     Collection<Customer> all(){
-        var customers = new ArrayList<Customer>();
-        try {
-            try (var connection = this.dataSource.getConnection()) {
-                try (var stmt = connection.createStatement()) {
-                    try (var rs = stmt.executeQuery("SELECT * FROM customers")) {
-                        while (rs.next()) {
-                            customers.add(new Customer(
-                                    rs.getInt("id"),
-                                    rs.getString("name")
-                            ));
-                        }
-                    }
-                }
-            }
-        }catch (Exception e){
-            log.error("something wrong", e);
-        }
-        return customers;
+        return this.template.query("select * from customers", this.rowMapper);
     }
+}
+
+class TransactionalDefaultCustomerService extends DefaultCustomerService {
+
+    TransactionalDefaultCustomerService(JdbcTemplate template, TransactionTemplate tt) {
+        super(template, tt);
+    }
+
+    @Override
+    Customer add(String name) {
+        return super.add(name);
+    }
+
+    @Override
+    Customer byId(Integer id) {
+        return super.byId(id);
+    }
+
+    @Override
+    Collection<Customer> all() {
+        return super.all();
+    }
+
+
 }
 
 record Customer (Integer id, String name) {}
---
+++ src/test/java/com/example/sb3/DefaultCustomerServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.sb3;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultCustomerServiceTest {
+
+    @Test
+    void all() throws Exception{
+//        var ds = Mockito.mock(DataSource.class);
+//        var cs = new DefaultCustomerService(ds);
+
+    }
+
+}
